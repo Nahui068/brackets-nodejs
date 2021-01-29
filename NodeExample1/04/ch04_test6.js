fe12e7509775feb54c4c46ac0f221b33e455d2cd
Nahui068 <kyjjj105@naver.com>
2021-01-29 11:10:44 +0900

    01.28

--- NodeExample1/04/ch04_test6.js
+++ NodeExample1/04/ch04_test6.js
@@ -1,7 +1,8 @@
-var fs = require('fs');
+var fs = require('fs'); // 파일을 읽어들이는 것 뿐만 아니라 파일을 쓰는 기능까지 fs모듈에 정의
 
 // 파일을 비동기식 IO로 읽어 들입니다.
 // readFile()메소드를 실행하면서 세 번째 파라미터로 전달된 함수는 파일을 읽어 들이는 작업이 끝났을 때 호출
+// 이때 두 개의 파라미터 err와 data를 전달받아서 오류가 발생했는지 아니면 제대로 실행되었는지 알 수 있음
 fs.readFile('./package.json','utf8',function(err,data){ 
     //읽어들인 데이터 출력
     console.log(data);
--- NodeExample1/04/ch04_test8.js
+++ NodeExample1/04/ch04_test8.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 // 파일에 데이터 씀
-fs.open('./output.txt','w',function(err,fd){
+fs.open('./output.txt','w',function(err,fd){ // 파일이름, 파일을 읽거나 쓰기 위한 플래그(쓸 것이기 때문에 w입력)
    if(err) throw err;
     
     var buf = new Buffer('안녕!\n');
